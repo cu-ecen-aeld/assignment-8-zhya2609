1efc2eea6928bdcee94680cb3942cad2130ae54f
zhya2609 <268274666+zhya2609@users.noreply.github.com>
2026-06-06 00:33:33 -0700

    update aesdchar driver and  aesdsocket

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,24 +5,34 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_VERSION = 35863ce907484f94347ad18b2f136f7844a86528
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-zhya2609.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) -C $(@D)/finder-app clean
+	$(MAKE) -C $(@D)/finder-app all CC=$(TARGET_CC) CROSS_COMPILE=$(TARGET_CROSS)
+	$(MAKE) -C $(@D)/server clean
+	$(MAKE) -C $(@D)/server all CC=$(TARGET_CC) CROSS_COMPILE=$(TARGET_CROSS)
+	$(MAKE) -C $(LINUX_DIR) M=$(@D)/aesd-char-driver modules $(TARGET_CONFIGURE_OPTS)
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer          $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh       $(TARGET_DIR)/usr/bin/finder.sh
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh  $(TARGET_DIR)/usr/bin/finder-test.sh
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket       $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(MAKE) -C $(LINUX_DIR) M=$(@D)/aesd-char-driver INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load  $(TARGET_DIR)/usr/bin/aesdchar_load
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/aesdchar_unload
+	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_PKGDIR)/S98aesdchar $(TARGET_DIR)/etc/init.d/S98aesdchar
 endef
 
 $(eval $(generic-package))
